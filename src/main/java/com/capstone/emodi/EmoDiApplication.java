448b80991a5c3e7c637b651f65e5c5656246e15e
GoGradually <chkun3109@gmail.com>
2024-05-07 16:23:17 +0900

    EmoDi Backend start

---
+++ src/main/java/com/capstone/emodi/EmoDiApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.emodi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmoDiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmoDiApplication.class, args);
+    }
+
+}
