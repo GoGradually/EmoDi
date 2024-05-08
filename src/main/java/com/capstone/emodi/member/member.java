7f42b3c2a584d7160707816cf0e3a9a99ac29deb
GoGradually <chkun3109@gmail.com>
2024-05-08 18:12:33 +0900

    멤버 엔티티 설계

---
+++ src/main/java/com/capstone/emodi/member/member.java
@@ -0,0 +1,34 @@
+package com.capstone.emodi.member;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+
+@Entity
+@Table(uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username")
+})
+public class member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+
+    @Column(nullable = false)
+    private String tellNumber;
+
+
+
+}
