54b843b88b48d1638fe4ceb50457dc2db5f805d9
GoGradually <chkun3109@gmail.com>
2024-05-29 20:14:50 +0900

    테스트 Access 토큰 발급용

--- src/test/java/com/capstone/emodi/EmoDiApplicationTests.java
+++ src/test/java/com/capstone/emodi/EmoDiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.capstone.emodi;
 
+import com.capstone.emodi.security.JwtTokenProvider;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class EmoDiApplicationTests {
-
+    @Autowired
+    JwtTokenProvider jwtTokenProvider;
     @Test
     void contextLoads() {
+        String accessToken = jwtTokenProvider.generateAccessToken("username");
+        log.info("token = {}", accessToken);
     }
 
 }
