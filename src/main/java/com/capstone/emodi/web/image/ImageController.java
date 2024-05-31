0f3cfb465d3ad17aa8b3ac9ac4a62b100f27bcff
GoGradually <chkun3109@gmail.com>
2024-05-31 14:13:07 +0900

    이미지 컨트롤러 정상 방식 리턴으로 변경

--- src/main/java/com/capstone/emodi/web/image/ImageController.java
+++ src/main/java/com/capstone/emodi/web/image/ImageController.java
@@ -4,12 +4,17 @@ package com.capstone.emodi.web.image;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @RestController
 @RequestMapping("/images")
@@ -23,19 +28,28 @@ public class ImageController {
     private String privatePostImageDir;
 
     @GetMapping("/profile/{filename}")
-    public Resource downloadProfileImage(@PathVariable String filename) throws
-            MalformedURLException {
-        return new UrlResource("file:" +profileImageDir + "/" + filename);
+    public ResponseEntity<Resource> downloadProfileImage(@PathVariable String filename) throws
+            IOException {
+        String mimeType = Files.probeContentType(Paths.get(profileImageDir, filename));
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(mimeType))
+                .body(new UrlResource("file:" +profileImageDir + "/" + filename));
     }
     @GetMapping("/post/{filename}")
-    public Resource downloadPostImage(@PathVariable String filename) throws
-            MalformedURLException {
-        return new UrlResource("file:" + postImageDir + "/" +  filename);
+    public ResponseEntity<Resource> downloadPostImage(@PathVariable String filename) throws
+            IOException {
+        String mimeType = Files.probeContentType(Paths.get(postImageDir, filename));
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(mimeType))
+                .body(new UrlResource("file:" +postImageDir + "/" + filename));
     }
 
     @GetMapping("/privatePost/{filename}")
-    public Resource downloadPrivatePostImage(@PathVariable String filename) throws
-            MalformedURLException {
-        return new UrlResource("file:" + privatePostImageDir + "/" + filename);
+    public ResponseEntity<Resource> downloadPrivatePostImage(@PathVariable String filename) throws
+            IOException {
+        String mimeType = Files.probeContentType(Paths.get(privatePostImageDir, filename));
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(mimeType))
+                .body(new UrlResource("file:" +privatePostImageDir + "/" + filename));
     }
 }
