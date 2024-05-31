a0884c33155766182a41e61312735f0ef0cb55b7
GoGradually <chkun3109@gmail.com>
2024-05-31 14:43:40 +0900

    이미지 컨트롤러 정상 방식 리턴으로 변경

--- src/main/java/com/capstone/emodi/web/image/ImageController.java
+++ src/main/java/com/capstone/emodi/web/image/ImageController.java
@@ -6,6 +6,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,7 +17,7 @@ import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
-@RestController
+@Controller
 @RequestMapping("/images")
 public class ImageController {
 
