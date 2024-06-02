0c0618bbeafb6d0cc42768d9c9787e528c5c7c1c
GoGradually <chkun3109@gmail.com>
2024-06-02 14:23:29 +0900

    CORS 허용 설정 추가

--- src/main/java/com/capstone/emodi/WebConfig.java
+++ src/main/java/com/capstone/emodi/WebConfig.java
@@ -1,7 +1,9 @@
 package com.capstone.emodi;
 
 import com.capstone.emodi.web.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,6 +16,17 @@ public class WebConfig implements WebMvcConfigurer {
                 .order(1)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/error");
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://emo-di.com")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE");
+            }
+        };
     }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
