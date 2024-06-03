d249c746ded9530d0effde9b319d46341cd880ae
GoGradually <chkun3109@gmail.com>
2024-06-03 12:26:41 +0900

    loginId 에 인덱스 적용

--- src/main/java/com/capstone/emodi/domain/member/Member.java
+++ src/main/java/com/capstone/emodi/domain/member/Member.java
@@ -27,7 +27,7 @@ public class Member {
     @Column(name = "member_id")
     private Long id;
 
-    @Column(nullable = false,unique = true, length = 20)
+    @Column(nullable = false, length = 20)
     private String loginId;
 
     @Column(nullable = false, length = 20)
